51d5986d74386e9f63947c9097fcd217c55260ee
Calvin-C-M <montolalu28@gmail.com>
2022-07-28 19:33:57 +0700

    Added menu list in index + installed react icons

--- components/MenuCard.js
+++ components/MenuCard.js
@@ -1,8 +1,18 @@
+import Link from "next/link";
+
 const MenuCard = ({ title,link,icon }) => {
     return (
-        <div id="menu-card" className="">
-            
-        </div>
+        <Link id={"menu-card-" + title} className="menu-card" href={link}>
+            <a className="flex flex-col bg-blue-800 p-1 border-2 border-black rounded-md shadow-lg">
+                <div className="px-8 py-10">
+                    {icon}
+                </div>
+                <hr />
+                <span className="text-lg m-auto border-solid">
+                    {title}
+                </span>
+            </a>
+        </Link>
     );
 }
  
--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,26 @@ import Image from 'next/image'
 import MenuCard from '../components/MenuCard'
 import styles from '../styles/Home.module.css'
 
+import { BiTask } from 'react-icons/bi'
+import { AiOutlineSetting, AiOutlineSchedule } from 'react-icons/ai'
+
 export default function Home() {
+  const iconClass="m-auto text-7xl"
+
   const cardMenu=[
-    { title: "", link: "" }
+    { title: "Jadwal", link: "/jadwal", icon: <AiOutlineSchedule className={iconClass} /> },
+    { title: "Tugas", link: "/tugas", icon: <BiTask className={iconClass} /> },
+    { title: "Settings", link: "/settings", icon: <AiOutlineSetting className={iconClass} /> }
   ]
 
   return (
-    <div id="home" className="text-3xl font-bold underline">
-      <div id="menus" className="">
-        <MenuCard title="Test" link="/" />
+    <div id="home" className="flex flex-col">
+      <h1 className='text-4xl font-bold'>Halo Sobat Ngosong!</h1>
+      <div id="menus" className='flex flex-col gap-3 mt-3'>
+        <h2 className='text-3xl font-bold'>Menu List</h2>
+        <div id="menulist" className="grid grid-cols-2 gap-4">
+          { cardMenu.map(card => <MenuCard title={card.title} link={card.link} icon={card.icon} />) }
+        </div>
       </div>
     </div>
   )
