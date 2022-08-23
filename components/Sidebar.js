cdfb642ce225c5c1ec178f963acb182d960537f1
Calvin-C-M <montolalu28@gmail.com>
2022-08-24 00:40:56 +0700

    Update UI for sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,4 +1,4 @@
-import ListNavbar from "./ListNavbar";
+import Link from "next/link";
 
 const Sidebar = () => {
     const menuList=[
@@ -12,8 +12,15 @@ const Sidebar = () => {
         <div className="drawer-side">
             <label for="my-drawer-2" className="drawer-overlay"></label> 
             <ul className="menu p-4 overflow-y-auto w-60 bg-base-100 text-base-content">
-                <li><a>Sidebar Item 1</a></li>
-                <li><a>Sidebar Item 2</a></li>
+                <h1 className="text-2xl">Ngosong Bot App</h1>
+                <div className="divider"></div>
+                {
+                    menuList.map(menu => <li>
+                        <Link href={menu.link}>
+                            <a>{menu.name}</a>
+                        </Link>
+                    </li>)
+                }
             </ul>
         </div>
     );
